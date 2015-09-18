b89b93fe8680a4f942ac285f1e83088c492e31fb
NYUTerminal <praveen.mareedu07@gmail.com>
2015-09-17 22:12:28 -0400

    able to get the whole webweb results from Flickr and return the HTML stream to output

--- hayFlickr.js
+++ hayFlickr.js
@@ -1,11 +1,27 @@
-var flickr = new Flickr({
-  api_key: "da68bb5e62015663c5cf069420da3a3e"
+var express = require('express');
+var app = express();
+var flikrSearchURL = "https://www.flickr.com/search/?text=";
+app.get('/', function(req, res) {
+  res.send('Trigger Flikr using Node\n');
 });
+app.get('/:search', function(req, res) {
 
-flickr.photos.search({
-  text: "sunset"
-}, function(err, result) {
-  if(err) { throw new Error(err); }
-  console.log()
-  // do something with result
-}
+   console.log("searching with the term " + req.params.search)
+   // console.log("final URL " + flikrSearchURL.concat(req.params.search));
+				var request = require('request');
+				request(flikrSearchURL.concat(req.params.search), function (error, response, body) {
+				    //Check for error
+				    if(error){
+				        return console.log('Error:', error);
+				    }
+				    //Check for right status code
+				    if(response.statusCode !== 200){
+				        return console.log('Invalid Status Code Returned:', response.statusCode);
+				    }
+				    // var parsed = JSON.parse(body);
+				    res.send(body); // Show the HTML for the Modulus homepage.
+				    // res.write(body);
+				});
+});
+app.listen(3001);
+console.log('Listening on port 3001...');
