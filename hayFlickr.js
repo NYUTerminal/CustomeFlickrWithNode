fcda90658748d1149a6c7fc7c3b1389d52a3a3ba
NYUTerminal <praveen.mareedu07@gmail.com>
2015-09-19 14:58:30 -0400

    final changes

--- hayFlickr.js
+++ hayFlickr.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var app = express();
-// var flikrSearchURL = "https://www.flickr.com/search/?text=";
 var api_key = "30451b7aa8e2337ba559dc414e63a45e";
 var auth_token = "72157658797079255-f2f7eca7c14bc647" //Optional
 var api_sig = "3b14f7c3a8929b2b11c808062f3a657b"//Optional
-//.concat("&auth_token=").concat(auth_token).concat("&api_sig=").concat(api_sig)
 var flickrSearchURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=".concat(api_key).concat("&format=json&nojsoncallback=1")  
+var flickrPhotoInfo =  "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=".concat(api_key).concat("&format=json&nojsoncallback=1")
+var flickrImage = "https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg"
 
-
-//https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a09bfd56f0e65b3f333ce4199260bca3&tags=panda&format=json&nojsoncallback=1&auth_token=72157658797079255-f2f7eca7c14bc647&api_sig=4adb97a90ff5a51142d7e426106110e3
-
+/*
+* 	General Search to see service is started or not.
+*/
 app.get('/', function(req, res) {
   res.send('Enter Search term after / to search in Flickr API \n');
 });
+
+/*
+* 	Flickr Search API invoked from the below code. 
+*/
 app.get('/:search', function(req, res) {
    console.log("searching with the term " + flickrSearchURL.concat("&tags=").concat(req.params.search))
 				var request = require('request');
@@ -23,14 +27,26 @@ app.get('/:search', function(req, res) {
 				    if(response.statusCode !== 200){
 				        return console.log('Invalid Status Code Returned:', response.statusCode);
 				    }
-				    res.send(response);
-				    // var parsed = JSON.parse(body);
-				    // res.send(parsed);
-				    // res.write(body);
+				    // console.log(body)
+				    var jsonObj = JSON.parse(body);						
+				    // console.log(jsonObj)
+					var imageLists = '<ul>';
+					for (var i = 0; i < jsonObj['photos']['photo'].length; i++) {
+						var obj = jsonObj['photos']['photo'][i];
+						var imageUrl = "https://farm".concat(obj['farm']).concat(".staticflickr.com/").concat(obj['server']).concat("/").concat(obj['id']).concat("_").concat(obj['secret']).concat("_m.jpg");
+						console.log(imageUrl);
+						// console.log(flickrPhotoInfo.concat("&photo_id==").concat(jsonObj['photos']['photo'][i]['id']));
+						imageLists += '<li  style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.5em;"><img src="' + imageUrl + '"></li>';
+					}
+		            imageLists += '</ul>';
+		            res.writeHead(200, {'Content-type':'text/html'});
+		            res.end(imageLists);
 				});
 });
 
-
+/*
+* 	Flickr Search with Number of results limited to the Count specified in the URL. 
+*/
 app.get('/:search/:count', function(req, res) {
    console.log("\nsearching with the term :" + req.params.search + " , max count : " + req.params.count)
 				var request = require('request');
@@ -41,13 +57,27 @@ app.get('/:search/:count', function(req, res) {
 				    if(response.statusCode !== 200){
 				        return console.log('Invalid Status Code Returned:', response.statusCode);
 				    }
-				    // var parsed = JSON.parse(body);
-				    // res.send(body); 
-				    res.send(response);
-				    // res.write(body);
+				    // console.log(body)
+				    var jsonObj = JSON.parse(body);
+					console.log(jsonObj['photos']['photo'][0]['id']);
+						
+					var imageLists = '<ul>';
+					for (var i = 0; i < jsonObj['photos']['photo'].length; i++) {
+						var obj = jsonObj['photos']['photo'][i];
+						var imageUrl = "https://farm".concat(obj['farm']).concat(".staticflickr.com/").concat(obj['server']).concat("/").concat(obj['id']).concat("_").concat(obj['secret']).concat("_m.jpg");
+						console.log(imageUrl);
+						// console.log(flickrPhotoInfo.concat("&photo_id==").concat(jsonObj['photos']['photo'][i]['id']));
+						imageLists += '<li style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.5em;"><img src="' + imageUrl + '"></li>';
+					}
+		            imageLists += '</ul>';
+		            res.writeHead(200, {'Content-type':'text/html'});
+		            res.end(imageLists);
 				});
 });
 
 
+/*
+	Port to which Node listens to 7001 . Could be configurable here .
+*/
 app.listen(7001);
 console.log('Listening on port 7001...');
